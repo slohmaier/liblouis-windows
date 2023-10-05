4300b4f251cf8ee6250132b8812c60bfd441fd8c
Stefan Lohmaier <stefan.lohmaier@eu.socionext.com>
2023-10-05 13:50:20 +0200

    ignore dist and working setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,4 @@ setup(
     package_data={'louis': ['liblouis.dll', 'tables/*']},
     include_package_data=True,
     # Add more metadata and dependencies as needed
-)from setuptools import setup, find_packages
-
-setup(
-    name='louis',
-    version='3.25.0',
-    packages=find_packages(),
-    package_data={'louis': ['liblouis.20.dylib', 'tables/*']},
-    include_package_data=True,
-    # Add more metadata and dependencies as needed
 )
